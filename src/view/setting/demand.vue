b4e586f1c92861bdf24e673e175071a7bc4f2ff2
Viuhety <405192400@qq.com>
2021-01-16 23:27:32 +0800

    fix

--- src/view/setting/demand.vue
+++ src/view/setting/demand.vue
@@ -11,6 +11,14 @@
           <a>{{ text }}</a>
         </span>
       </template>
+
+      <template #programmeList="{ text }">
+        <div v-for="item in text" :key="item">
+          <span>
+            <a>{{ item.programmeName }}{{ " " }}{{ item.branchName }}</a>
+          </span>
+        </div>
+      </template>
       <template #status="{ text: record }">
         <a>{{ record.status }}</a>
         <a-divider type="vertical" />
@@ -43,32 +51,111 @@
     <div class="addEditWindown">
       <a-modal
         :title="wtitle"
+        :width="900"
         v-model:visible="addEditVisible"
         :confirm-loading="confirmLoading"
         @ok="handleOk"
       >
         <a-form :model="demand" :label-col="labelCol" :wrapper-col="wrapperCol">
-          <a-form-item label="项目名称" has-feedback>
-            <a-select
-              v-model:value="demand.projectName"
-              placeholder="Please select a country"
+          <a-row>
+            <a-col :span="12">
+              <a-form-item label="项目名称" has-feedback>
+                <a-select
+                  v-model:value="demand.projectName"
+                  placeholder="Please select a country"
+                >
+                  <a-select-option value="china"> China </a-select-option>
+                  <a-select-option value="usa"> U.S.A </a-select-option>
+                </a-select>
+              </a-form-item>
+            </a-col>
+            <a-col :span="12">
+              <a-form-item label="需求名称">
+                <a-input
+                  placeholder="请输入项目名称"
+                  v-model:value="demand.demandName"
+                />
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-row>
+            <a-col :span="12">
+              <a-form-item label="计划开始时间">
+                <a-date-picker show-time placeholder="选择时间" /> </a-form-item
+            ></a-col>
+            <a-col :span="12">
+              <a-form-item label="计划上线时间">
+                <a-date-picker show-time placeholder="选择时间" />
+              </a-form-item>
+            </a-col>
+          </a-row>
+          <a-form-item label="工程/分支">
+            <a-table
+              :columns="programmeColumns"
+              :data-source="programmeList"
+              bordered
             >
-              <a-select-option value="china"> China </a-select-option>
-              <a-select-option value="usa"> U.S.A </a-select-option>
-            </a-select>
-          </a-form-item>
-          <a-form-item label="需求名称">
-            <a-input
-              placeholder="请输入项目名称"
-              v-model:value="demand.demandName"
-            />
-          </a-form-item>
+              <template #programmeName="{ text, record }">
+                <div>
+                  <a-input
+                    v-if="record.editable"
+                    style="margin: -5px 0"
+                    :value="text"
+                    @change="
+                      (e) => handleChangeProgrammeName(e.target.value, record)
+                    "
+                  />
+                  <template v-else>
+                    {{ text }}
+                  </template>
+                </div>
+              </template>
+              <template #branchName="{ text, record }">
+                <div>
+                  <a-input
+                    v-if="record.editable"
+                    style="margin: -5px 0"
+                    :value="text"
+                    @change="
+                      (e) => handleChangeBranchName(e.target.value, record)
+                    "
+                  />
+                  <template v-else> {{ text }} </template>
+                </div>
+              </template>
+              <template #action="{ text: record }">
+                <span>
+                  <span v-if="record.editable">
+                    <a-button
+                      type="primary"
+                      class="onSelect"
+                      @click="saveProgramme(record)"
+                      >确定</a-button
+                    >
+                  </span>
+                  <span v-else>
+                    <a-button
+                      type="primary"
+                      :disabled="programmeHasEdit"
+                      class="onSelect"
+                      @click="editProgramme(record)"
+                      >编辑</a-button
+                    >
+                  </span>
 
-          <a-form-item label="计划开始时间">
-            <a-date-picker show-time placeholder="选择时间" />
-          </a-form-item>
-          <a-form-item label="计划上线时间">
-            <a-date-picker show-time placeholder="选择时间" />
+                  <a-divider type="vertical" />
+                  <a-button
+                    type="primary"
+                    class="onSelect"
+                    @click="deleteProgramme(record)"
+                    >删除</a-button
+                  >
+                </span>
+              </template>
+            </a-table>
+            <a-config-provider :auto-insert-space-in-button="false">
+              <a-button type="primary" @click="addProgramme"> 添加 </a-button>
+            </a-config-provider>
           </a-form-item>
           <a-form-item label="需求信息">
             <a-textarea
@@ -115,6 +202,12 @@ const columns = [
     slots: { customRender: "demandInfo" },
   },
   {
+    title: "工程/分支",
+    //className: "column-projetInfo",
+    dataIndex: "programmeList",
+    slots: { customRender: "programmeList" },
+  },
+  {
     title: "状态",
     //className: "column-projetInfo",
     key: "action",
@@ -123,7 +216,7 @@ const columns = [
   {
     title: "更新记录",
     //className: "column-projetInfo",
-     key: "action",
+    key: "action",
     slots: { customRender: "updateInfo" },
   },
   //   {
@@ -145,10 +238,22 @@ const data = [
     demandName: "xxx优化",
     status: "未开始",
     demandInfo: "￥300,000.00",
+    programmeList: [
+      {
+        key: "1",
+        programmeName: "cloud-office-busi-finance",
+        branchName: "fix/5056",
+      },
+      {
+        key: "2",
+        programmeName: "cloud-office-busi-hr",
+        branchName: "fix/5056",
+      },
+    ],
     address: "New York No. 1 Lake Park",
   },
   {
-    key: "2",
+    id: "2",
     projectName: "xxx项目",
     demandName: "xx2优化",
     status: "未开始",
@@ -156,7 +261,7 @@ const data = [
     address: "London No. 1 Lake Park",
   },
   {
-    key: "3",
+    id: "3",
     projectName: "xxx项目",
     demandName: "Joe Black",
     status: "未开始",
@@ -165,6 +270,26 @@ const data = [
   },
 ];
 
+const programmeColumns = [
+  {
+    title: "工程名称",
+    dataIndex: "programmeName",
+    slots: { customRender: "programmeName" },
+  },
+  {
+    title: "分支名称",
+    dataIndex: "branchName",
+    slots: { customRender: "branchName" },
+  },
+  {
+    title: "操作",
+    key: "action",
+    slots: { customRender: "action" },
+  },
+];
+
+const programmeList = [];
+
 export default {
   name: "SettingDemand",
   data() {
@@ -172,6 +297,9 @@ export default {
       wtitle: "添加",
       data,
       columns,
+      programmeColumns,
+      programmeList,
+      programmeHasEdit: false,
       addEditVisible: false,
       confirmLoading: false,
       demand: {
@@ -188,15 +316,18 @@ export default {
       console.log(id);
     },
     editProject: function (row) {
+      console.log(this.programmeHasEdit);
+      this.programmeList = row.programmeList;
       this.showWindows("编辑项目", JSON.parse(JSON.stringify(row)));
     },
     addProjet() {
+      this.programmeList = [];
       this.showWindows("添加项目", {});
     },
     showWindows: function (title, row) {
       console.log(row);
       this.demand = row;
-      this.wtitle = "添加项目";
+      this.wtitle = title;
       this.addEditVisible = true;
     },
     handleOk() {
@@ -206,6 +337,42 @@ export default {
         this.addEditVisible = false;
         this.confirmLoading = false;
       }, 2000);
+    },
+    editProgramme(row) {
+      this.programmeHasEdit = true;
+      row.editable = true;
+    },
+    saveProgramme(row) {
+      console.log(row);
+      this.programmeHasEdit = false;
+      delete row.editable;
+    },
+    handleChangeProgrammeName(value, row) {
+      row.programmeName = value;
+    },
+    handleChangeBranchName(value, row) {
+      row.branchName = value;
+    },
+    addProgramme() {
+      if (this.programmeHasEdit) {
+        return;
+      }
+      this.programmeHasEdit = true;
+      if (!this.programmeList) {
+        this.programmeList = [];
+      }
+      this.programmeList.push({
+        id: "",
+        editable: true,
+        programmeName: "",
+        branchName: "",
+      });
+    },
+    deleteProgramme(row) {
+      console.log(row);
+      var target = this.programmeList.filter((item) => item != row);
+      this.programmeList = target;
+      this.programmeHasEdit = false;
     },
   },
 };
